e417f1ce463b88b904da8b81f0f96b52471f125c
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2026-01-22 20:53:13 -0500

    update configurations

--- backend/app/v1/core/configurations.py
+++ backend/app/v1/core/configurations.py
@@ -18,20 +18,28 @@ class ApplicationSettings(BaseSettings):
     load_dotenv()
     application_api_prefix: str = "/api/v1"
     application_debug_flag: bool = False
-    application_device: str = "cpu"
     application_description: str = "AI powered multimodal product search backend."
+    application_device: str = "cpu"
     application_name: str = "Retail Product Agent Backend"
     application_version: str = "1.0.0"
-    qdrant_url: str = os.getenv("qdrant_url")
-    redis_url: str = os.getenv("redis_url")
+    aws_access_key_id :str = os.getenv("aws_access_key_id")
+    aws_region: str = os.getenv("aws_region")
+    aws_s3_bucket_name: str = os.getenv("aws_s3_bucket_name")
+    aws_secret_access_key: str = os.getenv("aws_secret_access_key")
+    blip_model_name: str = os.getenv("blip_model_name")
+    clip_model_name: str = os.getenv("clip_model_name")
+    max_image_size: int = 4096
     postgres_database: str = os.getenv("postgres_database")
     postgres_host: str = os.getenv("postgres_host")
     postgres_password: str = os.getenv("postgres_password")
-    postgres_port: int = int(os.getenv("postgres_port", 5432))
+    postgres_port: int = int(os.getenv("postgres_port"))
     postgres_user: str = os.getenv("postgres_user")
-    supported_image_formats: set[str] = {"JPEG", "PNG", "GIF", "WEBP", "BMP"}
+    qdrant_collections: list[str] = os.getenv("qdrant_collections").split(",")
+    qdrant_url: str = os.getenv("qdrant_url")
+    redis_url: str = os.getenv("redis_url")
+    rerank_model_name: str = os.getenv("rerank_model_name")
     supported_image_extensions: set[str] = {".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp"}
-    max_image_size: int = 4096
+    supported_image_formats: set[str] = {"JPEG", "PNG", "GIF", "WEBP", "BMP"}
 
 
 @lru_cache
